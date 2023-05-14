b022bfaa73c432d3be84b357bcbdc3bf864fe6c8
Mahadi Dev <mahadi.dev.pm@gmail.com>
2023-05-14 22:57:51 +0600

    add hero section

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -1,35 +1,39 @@
-"use client";
-import React from "react";
-import { Avatar, Navbar, Dropdown } from "flowbite-react";
-import LanSelector from "./LanSelector";
+'use client';
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import React from 'react';
+import LanSelector from './LanSelector';
 
 const Navigation = () => {
-  return (
-    <Navbar fluid={false} rounded={true}>
-      <Navbar.Brand href="https://flowbite.com/">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite
-        </span>
-      </Navbar.Brand>
-      <div className="flex gap-8 md:order-2">
-        <LanSelector />
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="/navbars" active={true}>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="/navbars">About</Navbar.Link>
-        <Navbar.Link href="/navbars">Services</Navbar.Link>
-        <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+	return (
+		<Navbar
+			fluid={false}
+			rounded={true}
+			className="w-full fixed top-0 left-0 z-40 !bg-transparent"
+		>
+			<Navbar.Brand href="https://flowbite.com/">
+				<img
+					src="/assets/images/logo.png"
+					className="mr-3 h-6 sm:h-9"
+					alt="Flowbite Logo"
+				/>
+				<span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+					MDEVI
+				</span>
+			</Navbar.Brand>
+			<div className="flex gap-8 md:order-2">
+				<LanSelector />
+				<Navbar.Toggle />
+			</div>
+			<Navbar.Collapse>
+				<Navbar.Link href="/navbars" active={true}>
+					Home
+				</Navbar.Link>
+				<Navbar.Link href="/navbars">About</Navbar.Link>
+				<Navbar.Link href="/navbars">Services</Navbar.Link>
+				<Navbar.Link href="/navbars">Pricing</Navbar.Link>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 };
 
 export default Navigation;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite-react/**/*.js",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [require("flowbite/plugin")],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./node_modules/flowbite-react/**/*.js',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				primary: '#1a56db',
+				secondary: '#ff5757',
+			},
+			container: {
+				center: true,
+				padding: '1rem',
+			},
+		},
+	},
+	plugins: [require('flowbite/plugin')],
 };
